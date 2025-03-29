eeddc6e6e76d2e31c3782b704a927f05f2a7405c
gibix <gibix@inventati.org>
2025-03-29 20:06:38 +0100

    chore: bump update

--- server/src/api.rs
+++ server/src/api.rs
@@ -1,21 +1,8 @@
 use serde::{Deserialize, Serialize};
-use tss_esapi::structures::{Attest, Public, Signature};
 
 use crate::{NetworkConf, SystemConf, TorRelayConf};
 
 // Define the api common structures for client and server
-
-pub struct CreateRequest {
-    pub ak_public: Public,
-    pub ek_public: Public,
-}
-
-#[derive(Debug)]
-pub struct ValidateRequest {
-    pub ak_public: Public,
-    pub signature: Signature,
-    pub attest: Attest,
-}
 
 #[derive(Debug)]
 pub struct ConfigResponse {
