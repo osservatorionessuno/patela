386a389a00dc45158ad09cb5191b5f225c4fd5cc
gibix <gibix@inventati.org>
2025-10-17 10:19:54 +0200

    feat: remove mtls

--- client/build.rs
+++ client/build.rs
@@ -7,25 +7,21 @@ use std::{
 
 fn main() {
     println!("cargo::rerun-if-changed=templates/torrc");
-    println!("cargo::rerun-if-env-changed=PATELA_CLIENT_CERT");
 
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("const_gen.rs");
 
-    let server_ca_file = env::var("PATELA_CA_CERT").unwrap();
-    let client_key_cert_file = env::var("PATELA_CLIENT_CERT").unwrap();
-    let patela_server = env::var("PATELA_SERVER").unwrap();
-
     let torrc_file = "templates/torrc";
 
-    let server_ca = fs::read(server_ca_file).unwrap();
-    let client = fs::read(client_key_cert_file).unwrap();
+    let server_ca = match env::var("PATELA_CA_CERT").ok() {
+        Some(ca_file) => fs::read(ca_file).ok(),
+        None => None,
+    };
+
     let torrc = fs::read_to_string(torrc_file).unwrap();
 
     let const_declarations = [
-        const_declaration!(pub PATELA_SERVER = patela_server),
         const_declaration!(pub SERVER_CA = server_ca),
-        const_declaration!(pub CLIENT_KEY_CERT = client),
         const_declaration!(pub TORRC_TEMLPLATE = torrc),
     ]
     .join("\n");
--- client/src/api.rs
+++ client/src/api.rs
@@ -2,23 +2,25 @@ use std::time::Duration;
 
 use reqwest::{Client, tls};
 
-use crate::{CLIENT_KEY_CERT, SERVER_CA};
+use crate::SERVER_CA;
 
 const SERVER_TIMEOUT_SEC: u64 = 5;
 
 pub async fn build_client() -> Result<Client, reqwest::Error> {
-    let cert = reqwest::Certificate::from_pem(SERVER_CA)?;
-
-    // NOTE: the key cert is the concatenation of the two pem files
-    let client_key_cert = reqwest::Identity::from_pem(CLIENT_KEY_CERT)?;
-
-    reqwest::Client::builder()
+    let builder = reqwest::Client::builder()
         .use_rustls_tls()
         .min_tls_version(tls::Version::TLS_1_3)
         .tls_built_in_root_certs(false)
-        .add_root_certificate(cert)
-        .identity(client_key_cert)
         .https_only(true)
-        .timeout(Duration::from_secs(SERVER_TIMEOUT_SEC))
-        .build()
+        .timeout(Duration::from_secs(SERVER_TIMEOUT_SEC));
+
+    // If available use embedded Authority
+    let builder = if let Some(server_authority) = SERVER_CA {
+        let cert = reqwest::Certificate::from_pem(server_authority)?;
+        builder.add_root_certificate(cert)
+    } else {
+        builder
+    };
+
+    builder.build()
 }
